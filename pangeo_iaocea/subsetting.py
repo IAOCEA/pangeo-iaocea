8176668708dafa964a451090db6118a7bb0fabd2
Justus Magin <keewis@posteo.de>
2025-04-04 22:22:30 +0200

    simplify the code

--- pangeo_iaocea/subsetting.py
+++ pangeo_iaocea/subsetting.py
@@ -5,14 +5,16 @@ import cf_xarray  # noqa: F401
 def polygon_to_bounds(polygon):
     lon, lat = polygon.exterior.xy
 
-    lon_range = (np.min(lon), np.max(lon))
-    lat_range = (np.min(lat), np.max(lat))
+    lon_min = np.min(lon)
+    lon_max = np.max(lon)
+    lat_min = np.min(lat)
+    lat_max = np.max(lat)
 
-    return lon_range, lat_range
+    return lon_min, lat_min, lon_max, lat_max
 
 
 def subset_dataset(ds, bbox):
-    (lon_min, lon_max), (lat_min, lat_max) = polygon_to_bounds(bbox)
+    lon_min, lat_min, lon_max, lat_max = polygon_to_bounds(bbox)
 
     condition = (
         (ds.cf["longitude"] >= lon_min)
